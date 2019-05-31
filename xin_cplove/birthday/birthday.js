d1a80e0c8b55ad6fa0772ef7e1b0d84e86d4aee7
郭锦涛 <3044309295@qq.com>
2019-05-31 14:58:04 +0800

    加入动态

--- xin_cplove/birthday/birthday.js
+++ xin_cplove/birthday/birthday.js
@@ -5,6 +5,31 @@ Page({
   data: {
     logs: []
   },
+  data: {
+    dates: '请选择>',
+    times: '12:00',
+    objectArray: ['中国', '英国', '美国'],
+    index: 0,
+  },
+  bindTimeChange: function (e) {
+    console.log("谁哦按")
+    this.setData({
+      times: e.detail.value
+    })
+  },
+  //  点击日期组件确定事件  
+  bindDateChange: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      dates: e.detail.value
+    })
+  },
+  bindPickerChange: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
   onLoad: function () {
     this.setData({
       logs: (wx.getStorageSync('birthday') || []).map(log => {
--- xin_cplove/site/site.js
+++ xin_cplove/site/site.js
@@ -5,6 +5,15 @@ Page({
   data: {
     logs: []
   },
+  data: { 
+  countryList: ['中国','美国','英国','日本','韩国','巴西','德国'], countryIndex: 6, 
+  region: ["请", "选", "择"], 
+  multiArray: [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]], multiIndex: [3,5], 
+  multiArray3: [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]], multiIndex3: [3, 5, 4] },
+  changeCountry(e) { this.setData({ countryIndex: e.detail.value }); }, 
+  changeRegin(e){ this.setData({ region: e.detail.value }); }, 
+  changeMultiPicker(e) { this.setData({multiIndex: e.detail.value}) },
+  changeMultiPicker3(e) { this.setData({ multiIndex3: e.detail.value }) },
   onLoad: function () {
     this.setData({
       logs: (wx.getStorageSync('site') || []).map(log => {
