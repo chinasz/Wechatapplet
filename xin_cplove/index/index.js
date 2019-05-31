aec27e02418da8ed747b065185711f52fad5ed24
sz <1053700416@qq.com>
2019-05-31 09:46:17 +0800

    调整目录结构

--- xin_cplove/index/index.js
+++ xin_cplove/index/index.js
@@ -3,17 +3,20 @@ const app = getApp();
 Page({
   data: {},
   bindGetUserInfo:()=>{
-    app.util.user((res)=>{
-      console.log(res);
-      app.util.request({
-        url: 'entry/wxapp/login',
-        method: 'post',
-        data: { uid: res.memberInfo.uid},
-        success: function (res) {
-          console.log(res);
-        }
-      })
-    });
+    // app.util.user((res)=>{
+    //   console.log(res);
+    //   app.util.request({
+    //     url: 'entry/wxapp/login',
+    //     method: 'post',
+    //     data: { uid: res.memberInfo.uid},
+    //     success: function (res) {
+    //       console.log(res);
+    //     }
+    //   })
+    // });
+    wx.navigateTo({
+      url: '../logs/logs',
+    })
   },
   onLoad: function () {
     
