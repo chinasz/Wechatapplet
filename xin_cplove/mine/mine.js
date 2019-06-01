918b11c5419819a288d204117644e6b553905902
郭锦涛 <3044309295@qq.com>
2019-06-01 14:29:35 +0800

    2019/6/1

--- xin_cplove/mine/mine.js
+++ xin_cplove/mine/mine.js
@@ -5,6 +5,26 @@ Page({
   data: {
     logs: []
   },
+  go:function(){
+    wx.redirectTo({
+      url: '../material/material',
+    })
+  },
+  go1: function () {
+    wx.redirectTo({
+      url: '../tone/tone',
+    })
+  },
+  go2: function () {
+    wx.redirectTo({
+      url: '../label/label',
+    })
+  },
+  go3: function () {
+    wx.redirectTo({
+      url: '../mindful/mindful',
+    })
+  },
   onLoad: function () {
     this.setData({
       logs: (wx.getStorageSync('mine') || []).map(log => {
