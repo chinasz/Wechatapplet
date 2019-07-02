784083720a7c8f289e199aac5fe6d5a27b25f6cf
sz <1053700416@qq.com>
2019-07-02 17:32:12 +0800

    报告故障

--- xin_cplove/report/report.js
+++ xin_cplove/report/report.js
@@ -1,15 +1,111 @@
-//logs.js
-const util = require('../../utils/util.js')
-
+//report.js
+const app = getApp();
 Page({
   data: {
-    logs: []
+    preview:[],
+    text:null,
+    count:9,
+    len:300,
+    img:[],
+    submit:true,
+  },
+  selectimg:function(){
+    //选择图片
+    var that = this;
+    var currentcount = that.data.preview.length || 0;
+    if (currentcount <that.data.count){
+      wx.chooseImage({
+        count: that.data.count - currentcount,
+        success(res){  
+          that.setData({
+            preview: that.data.preview.concat(res.tempFilePaths)
+          })
+        }
+      })
+    }
+  },
+  buginput: function (e) {
+    var that = this;
+    var bug = '';
+    if(e.detail.value.trim()){
+      bug = e.detail.value.trim();
+    }
+    that.setData({
+      len: 300 - e.detail.value.length,
+      text:bug
+    })
+  },
+  uploadimgs:function(data,cb,i=0){
+    wx.showLoading({
+      title: '提交中...',
+    })
+    var that = this;
+    wx.uploadFile({
+      url: '' + app.util.url('entry/wxapp/reportupload') + '&state=we7sid-' + wx.getStorageSync('userInfo').sessionid + '&m=xin_cplove',
+      filePath: data[i],
+      name: 'file',
+      success: function (res) {
+        res.data = JSON.parse(res.data)
+        if(res.data.errno == 0){
+          that.data.img.push(res.data.data)
+          that.setData({
+            img:that.data.img
+          })
+        } 
+      },
+      complete:function(){
+        i++;
+        if(i < data.length){
+          that.uploadimgs(data,cb,i)
+        }else{
+          wx.hideLoading();
+          cb()
+        }
+      }
+    })
+  },
+  bug:function(data){
+    var that = this;
+    app.util.request({
+      url: 'entry/wxapp/feedback',
+      method:'post',
+      data:data,
+      success:function(res){
+        if(res.data.errno == 0){
+          wx.navigateBack()
+          wx.showToast({
+            title: '感谢您的反馈',
+            icon:'none'
+          })
+        }
+      },
+    })
+  },
+  subbug:function(){
+    //提交故障
+    var that = this;
+    var imgupload = [];
+    if(that.data.text && that.data.submit){
+      that.setData({
+        submit:false
+      })
+      if(that.data.preview){
+        //循环图片上传
+        that.uploadimgs(that.data.preview,function(){
+          that.bug({text:that.data.text,img:that.data.img.join(','),op:'bug'})
+        },0)
+      }else{
+        that.bug({ text: that.data.text,op:'bug'})
+      }
+    }else{
+      wx.showToast({
+        title: '文本不能为空',
+        icon:'none'
+      })
+    }
+
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('report') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
+
   }
 })
